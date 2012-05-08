6e7bfd5473f82ef1626f55532a2a73533a09c3a0
Phil Cohen <github@phlippers.net>
2012-05-08 11:21:49 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "github@phlippers.net"
 license           "Apache 2.0"
 description       "Installs Percona MySQL client and server"
 long_description  "Please refer to README.md"
-version           "0.4.0"
+version           "0.4.1"
 
 recipe "percona",          "Sets up the apt repository and installs dependent packages"
 recipe "percona::client",  "Installs client libraries"
@@ -14,9 +14,7 @@ recipe "percona::toolkit", "Installs the Percona Toolkit software"
 recipe "percona::access_grants", "Used internally to grant permissions for recipes"
 recipe "percona::cluster", "Installs the Percona XtraDB Cluster server components"
 
-%w{ apt }.each do |cb|
-  depends cb
-end
+depends "apt"
 
 %w[debian ubuntu].each do |os|
   supports os

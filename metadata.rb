e8f3509e8a728e893af824b3859c223e91e40df7
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-09-03 20:24:28 -0400

    Moved default recipe into explicit package repo recipe, adding yum support.

--- metadata.rb
+++ metadata.rb
@@ -6,18 +6,20 @@ description       "Installs Percona MySQL client and server"
 long_description  "Please refer to README.md"
 version           "0.6.0"
 
-recipe "percona",          "Sets up the apt repository and installs dependent packages"
-recipe "percona::client",  "Installs client libraries"
-recipe "percona::server",  "Installs the server daemon"
-recipe "percona::backup",  "Installs the XtraBackup hot backup software"
-recipe "percona::toolkit", "Installs the Percona Toolkit software"
-recipe "percona::cluster", "Installs the Percona XtraDB Cluster server components"
-recipe "percona::replication", "Used internally to grant permissions for replication."
+recipe "percona",                "Default no-op recipe"
+recipe "percona::package_repo",  "Sets up the package repository and installs dependent packages"
+recipe "percona::client",        "Installs client libraries"
+recipe "percona::server",        "Installs the server daemon"
+recipe "percona::backup",        "Installs the XtraBackup hot backup software"
+recipe "percona::toolkit",       "Installs the Percona Toolkit software"
+recipe "percona::cluster",       "Installs the Percona XtraDB Cluster server components"
+recipe "percona::replication",   "Used internally to grant permissions for replication."
 recipe "percona::access_grants", "Used internally to grant permissions for recipes"
 
 depends "apt"
+depends "yum"
 depends "openssl"
 
-%w[debian ubuntu].each do |os|
+%w[debian ubuntu centos amazon scientific fedora redhat].each do |os|
   supports os
 end

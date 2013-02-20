05b0402deb7c6937fca9faf706353f4b913dc2e6
Sergio Rodriguez <srodrig0209@gmail.com>
2013-02-20 17:44:11 -0500

    This attribute isn't used in installing the percona monitoring plugins.

--- attributes/monitoring.rb
+++ attributes/monitoring.rb
@@ -20,4 +20,3 @@ default['percona']['plugins_url'] = "http://www.percona.com/downloads/percona-mo
 default['percona']['plugins_version'] = "1.0.2"
 default['percona']['plugins_sha'] = "da84cfe89637292da15ddb1e66f67ad9703fa21392d8d49e664ad08f7aa45585"
 default['percona']['plugins_path'] = "/opt/pmp"
-# default['percona']['plugins_nagios'] = "/opt/pmp/nagios/bin"

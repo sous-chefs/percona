e14c9c9e92b41fbeb2dc6f6397a4d0dc852e2ab6
Robert Starsi <klevo@klevo>
2013-07-10 19:16:08 +0200

    Percona repositories are added at compile time, due to `chef_gem` command installing gems at compile time. For example `chef_gem mysql` requires us to have percona-server-client installed at compile time - this can only happen if the percona repositories are there as well.

--- recipes/package_repo.rb
+++ recipes/package_repo.rb
@@ -4,16 +4,18 @@
 #
 
 case node['platform_family']
+  
+# Repositories need to be added at Chef's Compile time using .run_action(:add), due to `chef_gem` command installing gems at compile time. For example `chef_gem 'mysql'` requires us to have percona-server-client installed at compile time - this can only happen if the percona repositories are there as well.
 
 when "debian"
   include_recipe "apt"
-
+  
   # Pin this repo as to avoid upgrade conflicts with distribution repos.
   apt_preference "00percona" do
     glob "*"
     pin "release o=Percona Development Team"
     pin_priority "1001"
-  end
+  end.run_action(:add)
 
   apt_repository "percona" do
     uri node['percona']['apt_uri']
@@ -21,16 +23,14 @@ when "debian"
     components [ "main" ]
     keyserver node['percona']['apt_keyserver']
     key node['percona']['apt_key_id']
-    action :add
-  end
+  end.run_action(:add)
 
 when "rhel"
   include_recipe "yum"
 
   yum_key "RPM-GPG-KEY-percona" do
     url "http://www.percona.com/downloads/RPM-GPG-KEY-percona"
-    action :add
-  end
+  end.run_action(:add)
 
   arch = node['kernel']['machine'] == "x86_64" ? "x86_64" : "i386"
   pversion = node['platform_version'].to_i
@@ -39,7 +39,6 @@ when "rhel"
     description "Percona Repo"
     url "http://repo.percona.com/centos/#{pversion}/os/#{arch}/"
     key "RPM-GPG-KEY-percona"
-    action :add
-  end
+  end.run_action(:add)
 
 end

f39b736860de7ebda0a726a8acc4d4d3d7bd6a00
Tim Kuhlman <tim.kuhlman@hp.com>
2014-02-25 11:20:21 -0700

    Fixed an issue with failure to restart mysql on initial provisioning.

--- recipes/cluster.rb
+++ recipes/cluster.rb
@@ -3,7 +3,16 @@ include_recipe "percona::package_repo"
 # install packages
 case node["platform_family"]
 when "debian"
-  package node["percona"]["cluster"]["package"]
+  # The package start is up immediately, then additional config is added and the restart command fails to work.
+  # Instead stop the db before changing the config.
+  execute "stop_mysql" do
+    action :nothing
+    command '/etc/init.d/mysql stop'
+  end
+
+  package node["percona"]["cluster"]["package"] do
+    notifies :run, "execute[stop_mysql]", :immediately
+  end
 when "rhel"
   package "mysql-libs" do
     action :remove

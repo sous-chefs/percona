3b15546c28f8effbec21dbcb5011e8a3dfd5a7e0
Tim Kuhlman <tim.kuhlman@hp.com>
2014-02-25 11:01:09 -0700

    Added cluster package attribute, apt attribute and undeprecated cluster
    The apt attribute allows skipping the percona apt setup. Useful for
    environments where there is not outside internet access and all apt
    packages are via local mirrors.

--- recipes/cluster.rb
+++ recipes/cluster.rb
@@ -3,15 +3,13 @@ include_recipe "percona::package_repo"
 # install packages
 case node["platform_family"]
 when "debian"
-  package "percona-xtradb-cluster-server-5.5" do
-    options "--force-yes"
-  end
+  package node["percona"]["cluster"]["package"]
 when "rhel"
   package "mysql-libs" do
     action :remove
   end
 
-  package "Percona-XtraDB-Cluster-server"
+  package node["percona"]["cluster"]["package"]
 end
 
 include_recipe "percona::configure_server"

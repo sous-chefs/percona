ca161b0e0da48192525eb4279d3a5661fdb04a46
Darren Birkett <darren.birkett@gmail.com>
2014-02-27 16:14:11 +0000

    install monitoring plugins from package instead of tarball

--- attributes/monitoring.rb
+++ attributes/monitoring.rb
@@ -16,7 +16,5 @@
 # limitations under the License.
 #
 
-default['percona']['plugins_version'] = version = "1.0.2"
-default['percona']['plugins_url'] = "http://www.percona.com/downloads/percona-monitoring-plugins/#{version}/"
-default['percona']['plugins_sha'] = "da84cfe89637292da15ddb1e66f67ad9703fa21392d8d49e664ad08f7aa45585"
-default['percona']['plugins_path'] = "/opt/pmp"
+default['percona']['plugins_version'] = "1.0.2"
+default['percona']['plugins_packages'] = ['percona-monitoring-plugins']

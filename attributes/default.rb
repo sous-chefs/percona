9055836b6ba021759453fa263ce8146235adc2dc
achied <achied@gmail.com>
2014-03-14 11:30:50 +0100

    Fixing XtraBackup password settings

--- attributes/default.rb
+++ attributes/default.rb
@@ -148,7 +148,7 @@ default["percona"]["server"]["replication"]["ignore_db"]        = ""
 # XtraBackup Settings
 default["percona"]["backup"]["configure"]                       = false
 default["percona"]["backup"]["username"]                        = "backup"
-unless defined?(node["percona"]["backup"]["password"])
+unless attribute?(node["percona"]["backup"]["password"])
   default["percona"]["backup"]["password"]                      = secure_password
 end
 

f26a08d6316c19e59b320ea8f69a10b23403429e
Andrew Vit <andrew@avit.ca>
2014-04-11 19:21:45 -0700

    Remove dependency on mysql recipe

--- attributes/client.rb
+++ attributes/client.rb
@@ -1,6 +1,10 @@
+client = default["percona"]["client"]
+
 case node["platform_family"]
 when "debian"
-  normal['mysql']['client']['packages'] = %w{libmysqlclient-dev percona-server-client-5.5}
+  client["packages"] = ["libperconaserverclient-dev-5.5",
+                        "percona-server-client-5.5"]
 when "rhel"
-  normal['mysql']['client']['packages'] = %w{Percona-Server-client-55}
+  client["packages"] = ["Percona-Server-devel-55",
+                        "Percona-Server-client-55"]
 end

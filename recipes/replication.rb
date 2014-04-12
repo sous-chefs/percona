ecfd62752a4e9c37bd98ec6908004b751bb8b23f
Andrew Vit <andrew@avit.ca>
2014-04-11 17:44:10 -0700

    Simplify password usage for mysql client

--- recipes/replication.rb
+++ recipes/replication.rb
@@ -1,3 +1,5 @@
+require 'shellwords'
+
 passwords = EncryptedPasswords.new(node, node["percona"]["encrypted_data_bag"])
 
 server = node.percona.server
@@ -12,17 +14,11 @@ template "/etc/mysql/replication.sql" do
   only_if { server.replication.host != "" or server.role == "master" }
 end
 
-# execute access grants
-if passwords.root_password && !passwords.root_password.empty?
-  execute "mysql-set-replication" do
-    command "/usr/bin/mysql -p'" + passwords.root_password + "' -e '' &> /dev/null > /dev/null &> /dev/null ; if [ $? -eq 0 ] ; then /usr/bin/mysql -p'" + passwords.root_password + "' < /etc/mysql/replication.sql ; else /usr/bin/mysql < /etc/mysql/replication.sql ; fi ;"
-    action :nothing
-    subscribes :run, resources("template[/etc/mysql/replication.sql]"), :immediately
-  end
-else
-  execute "mysql-set-replication" do
-    command "/usr/bin/mysql < /etc/mysql/replication.sql"
-    action :nothing
-    subscribes :run, resources("template[/etc/mysql/replication.sql]"), :immediately
-  end
+root_pass = passwords.root_password.to_s
+root_pass = Shellwords.escape(root_pass).prepend('-p') unless root_pass.empty?
+
+execute "mysql-set-replication" do
+  command "/usr/bin/mysql #{root_pass} < /etc/mysql/replication.sql"
+  action :nothing
+  subscribes :run, resources("template[/etc/mysql/replication.sql]"), :immediately
 end

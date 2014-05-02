da352ae862fdf61810192fd00866741c017cadc3
Andrew Vit <andrew@avit.ca>
2014-05-02 13:41:13 -0700

    Clean up version attributes
    Rename mysql.version to percona.version to keep in our own namespace.
    Use variable instead of hard-coded version in cluster recipe.
    Update default version to 5.6

--- attributes/client.rb
+++ attributes/client.rb
@@ -1,10 +1,15 @@
+version = value_for_platform_family(
+  "debian" => node["percona"]["version"],
+  "rhel" => node["percona"]["version"].tr(".", "")
+)
+
 case node["platform_family"]
 when "debian"
-  default["percona"]["client"]["packages"] = %w[
-    libperconaserverclient-dev-5.5 percona-server-client-5.5
+  default["percona"]["client"]["packages"] = %W[
+    libperconaserverclient-dev-#{version} percona-server-client-#{version}
   ]
 when "rhel"
-  default["percona"]["client"] = %w[
-    Percona-Server-devel-55 Percona-Server-client-55
+  default["percona"]["client"] = %W[
+    Percona-Server-devel-#{version} Percona-Server-client-#{version}
   ]
 end

c21cbf57738f3f1b1f98c236fa05e51e8caee755
Andrew Vit <andrew@avit.ca>
2014-05-07 10:40:29 -0700

    Override mysql-chef_gem to skip installing mysql client libs

---
+++ libraries/provider_mysql_chef_gem.rb
@@ -0,0 +1,23 @@
+class Chef
+  class Provider
+    class MysqlChefGem < Chef::Provider::LWRPBase
+
+      # Monkey patch to not install mysql client dev libraries over ours
+      # https://github.com/opscode-cookbooks/mysql/blob/master/libraries/provider_mysql_client_ubuntu.rb
+      #
+      def action_install
+        converge_by 'install mysql chef_gem and dependencies' do
+          recipe_eval do
+            run_context.include_recipe 'build-essential::default'
+            run_context.include_recipe 'percona::client'
+          end
+
+          chef_gem 'mysql' do
+            action :install
+          end
+        end
+      end
+
+    end
+  end
+end

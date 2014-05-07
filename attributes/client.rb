e75b46372f8f9b7ba88f6da9310dab39a7d9ed0d
Andrew Vit <andrew@avit.ca>
2014-05-07 10:39:38 -0700

    Fix odd package names on ubuntu trusty

--- attributes/client.rb
+++ attributes/client.rb
@@ -3,8 +3,14 @@ version = node["percona"]["version"]
 
 case node["platform_family"]
 when "debian"
-  client["packages"] = ["libperconaserverclient-dev-#{version}",
-                        "percona-server-client-#{version}"]
+  if node["lsb"]["codename"] == "trusty"
+    abi_version = case version
+                  when "5.5" then "18"
+                  when "5.6" then "18.1"
+                  end
+  end
+  client["packages"] = ["libperconaserverclient#{abi_version}-dev",
+                        "percona-server-client"]
 when "rhel"
   client["packages"] = ["Percona-Server-devel-#{version.tr(',', '')}",
                         "Percona-Server-client-#{version.tr(',', '')}"]

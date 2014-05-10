247e9531f3ddd347e91288fee236eb6aeb67118d
Phil Cohen <github@phlippers.net>
2014-05-10 10:50:20 -0700

    style cleanup, make tests pass

--- recipes/replication.rb
+++ recipes/replication.rb
@@ -1,28 +1,25 @@
-require 'shellwords'
+require "shellwords"
 
 passwords = EncryptedPasswords.new(node, node["percona"]["encrypted_data_bag"])
 replication_sql = "/etc/mysql/replication.sql"
-server = node.percona.server
+server = node["percona"]["server"]
 
 # define access grants
 template replication_sql do
-  source "replication_#{server.role}.sql.erb"
+  source "replication_#{server["role"]}.sql.erb"
   variables(replication_password: passwords.replication_password)
   owner "root"
   group "root"
   mode "0600"
 
-  only_if do
-    node["percona"]["server"]["replication"]["host"] != "" ||
-      node["percona"]["server"]["role"] == "master"
-  end
+  only_if { server["replication"]["host"] != "" || server["role"] == "master" }
 end
 
 root_pass = passwords.root_password.to_s
-root_pass = Shellwords.escape(root_pass).prepend('-p') unless root_pass.empty?
+root_pass = Shellwords.escape(root_pass).prepend("-p") unless root_pass.empty?
 
 execute "mysql-set-replication" do
-  command "/usr/bin/mysql #{root_pass} < /etc/mysql/replication.sql"
+  command "/usr/bin/mysql #{root_pass} < #{replication_sql}"
   action :nothing
-  subscribes :run, resources("template[/etc/mysql/replication.sql]"), :immediately
+  subscribes :run, resources("template[#{replication_sql}]"), :immediately
 end

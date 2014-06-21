54c0287e70139b943f1127dd56a71f7c6d18a8a4
Phil Cohen <github@phlippers.net>
2014-06-21 15:26:48 -0700

    style updates

--- spec/toolkit_spec.rb
+++ spec/toolkit_spec.rb
@@ -1,13 +1,21 @@
 require "spec_helper"
 
 describe "percona::toolkit" do
+  let(:centos_package) do
+    "Percona-Server-shared-compat"
+  end
+
+  let(:ubuntu_package) do
+    "percona-toolkit"
+  end
+
   describe "Ubuntu" do
     let(:chef_run) do
       ChefSpec::Runner.new.converge(described_recipe)
     end
 
-    it { expect(chef_run).to_not install_package("Percona-Server-shared-compat") }
-    it { expect(chef_run).to install_package("percona-toolkit") }
+    it { expect(chef_run).to_not install_package(centos_package) }
+    it { expect(chef_run).to install_package(ubuntu_package) }
   end
 
   describe "CentOS" do
@@ -16,7 +24,7 @@ describe "percona::toolkit" do
       ChefSpec::Runner.new(env_options).converge(described_recipe)
     end
 
-    it { expect(chef_run).to install_package("Percona-Server-shared-compat") }
-    it { expect(chef_run).to install_package("percona-toolkit") }
+    it { expect(chef_run).to install_package(centos_package) }
+    it { expect(chef_run).to install_package(ubuntu_package) }
   end
 end

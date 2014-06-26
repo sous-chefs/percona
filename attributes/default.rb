3811ba0d17a68ab7f8663a743b402e1d23f7f771
Andrian Jardan <andrian@assembla.com>
2014-06-26 17:17:34 +0300

    Add SSL support for replication

--- attributes/default.rb
+++ attributes/default.rb
@@ -149,6 +149,7 @@ default["percona"]["server"]["replication"]["username"]         = ""
 default["percona"]["server"]["replication"]["password"]         = ""
 default["percona"]["server"]["replication"]["port"]             = 3306
 default["percona"]["server"]["replication"]["ignore_db"]        = ""
+default["percona"]["server"]["replication"]["ssl_enabled"]      = false
 
 # XtraBackup Settings
 default["percona"]["backup"]["configure"]                       = false

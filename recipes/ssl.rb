f9d452e8561a3e179e031b53a7419996ef6a7f38
Andrian Jardan <andrian@assembla.com>
2014-08-01 11:28:10 +0100

    Convert role to array so we can have more than 1

--- recipes/ssl.rb
+++ recipes/ssl.rb
@@ -21,14 +21,14 @@ end
 
 %w(cert key).each do |file|
   # place certificate and key for master
-  if server["role"] == 'master'
+  if server["role"].include?('master')
     file "#{certs_path}/server-#{file}.pem" do
       content certs['server']["server-#{file}"]
     end
   end
   # because in a master-master setup a slave could also be a master, we don't use else here
   # place slave certificate and key
-  if server["role"] == 'slave'
+  if server["role"].include?('slave')
     file "#{certs_path}/client-#{file}.pem" do
       content certs['client']["client-#{file}"]
     end

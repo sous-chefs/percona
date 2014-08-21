fcffeb5bd8236ee9553c2c1f84f77ce7b78bfe09
Phil Cohen <github@phlippers.net>
2014-08-21 14:45:02 -0700

    add spec coverage for config file location

--- spec/configure_server_spec.rb
+++ spec/configure_server_spec.rb
@@ -143,4 +143,27 @@ describe "percona::configure_server" do
       expect(resource).to notify("service[mysql]").to(:restart).immediately
     end
   end
+
+  describe "`rhel` platform family" do
+    let(:chef_run) do
+      env_options = { platform: "centos", version: "6.5" }
+      ChefSpec::Runner.new(env_options).converge(described_recipe)
+    end
+
+    before do
+      stub_command("test -f /var/lib/mysql/mysql/user.frm").and_return(false)
+      stub_command("test -f /etc/mysql/grants.sql").and_return(false)
+    end
+
+    it "creates the main server config file" do
+      expect(chef_run).to create_template("/etc/my.cnf").with(
+        owner: "root",
+        group: "root",
+        mode: "0644"
+      )
+
+      resource = chef_run.template("/etc/my.cnf")
+      expect(resource).to notify("service[mysql]").to(:restart).immediately
+    end
+  end
 end

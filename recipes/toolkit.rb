43e649fcce142e7144283265fa3f44d0f4f1a8f7
Jim Park <jpark@jim80.net>
2014-11-11 02:04:20 +0000

    percona::toolkit fails to install version 5.6

--- recipes/toolkit.rb
+++ recipes/toolkit.rb
@@ -8,7 +8,7 @@ include_recipe "percona::package_repo"
 # Workaround a bug in the RPM packaging of percona-toolkit. Otherwise, it'll
 #   try to pull in Percona-Server-shared-51, which will conflict with 5.5.
 # https://bugs.launchpad.net/percona-toolkit/+bug/1031427
-package "Percona-Server-shared-compat" if platform_family?("rhel")
+package "Percona-Server-shared-compat" if platform_family?("rhel") and /5\.[15]/ =~ node["percona"]["version"]
 
 package "percona-toolkit" do
   options "--force-yes" if platform_family?("debian")

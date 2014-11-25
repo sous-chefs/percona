ed5bf76a97db4a877bff227f5dfb2d0c57dfc36e
helgi <helgith@gmail.com>
2014-11-25 11:21:42 -0800

    Move from mysqladmin version instead of ping instead if does proper exit code

--- recipes/configure_server.rb
+++ recipes/configure_server.rb
@@ -120,7 +120,7 @@ unless node["percona"]["skip_passwords"]
   execute "Update MySQL root password" do
     root_pw = passwords.root_password
     command "mysqladmin --user=root --password='' password '#{root_pw}'"
-    only_if "mysqladmin --user=root --password='' ping"
+    only_if "mysqladmin --user=root --password='' version"
   end
 end
 

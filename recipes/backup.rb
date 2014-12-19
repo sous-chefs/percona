Sauraus/chef-percona

Install and configure pam_auth

recipes/backup.rb
@@ -18,3 +18,5 @@ end
 
 # access grants
 include_recipe "percona::access_grants" unless node["percona"]["skip_passwords"]
+# pam_auth config
+include_recile "percona::pam_auth" if node["percona"]["enable_pamauth"]

recipes/cluster.rb
@@ -37,3 +37,5 @@ end
 
 # access grants
 include_recipe "percona::access_grants" unless node["percona"]["skip_passwords"]
+# pam_auth config
+include_recile "percona::pam_auth" if node["percona"]["enable_pamauth"]

sous-chefs/percona

recipes/backup.rb
@@ -18,3 +18,5 @@ end
 
 # access grants
 include_recipe "percona::access_grants" unless node["percona"]["skip_passwords"]
+# pam_auth config
+include_recipe "percona::pam_auth" if node["percona"]["enable_pamauth"]

recipes/cluster.rb
@@ -48,3 +48,5 @@ end
 
 # access grants
 include_recipe "percona::access_grants" unless node["percona"]["skip_passwords"]
+# pam_auth config
+include_recipe "percona::pam_auth" if node["percona"]["enable_pamauth"]
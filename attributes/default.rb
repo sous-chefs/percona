1f4aa91b46071a7600530a057423158d005094b0
Arne Sund <arnesund@users.noreply.github.com>
2015-01-21 13:31:41 +0000

    Add support for chef-vault for passwords

--- attributes/default.rb
+++ attributes/default.rb
@@ -35,6 +35,7 @@ default["percona"]["main_config_file"]                          = value_for_plat
 default["percona"]["keyserver"]                                 = "keys.gnupg.net"
 default["percona"]["encrypted_data_bag"]                        = "passwords"
 default["percona"]["encrypted_data_bag_secret_file"]            = ""
+default["percona"]["use_chef-vault"]                            = false
 default["percona"]["skip_passwords"]                            = false
 default["percona"]["skip_configure"]                            = false
 

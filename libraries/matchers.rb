929837c1036eabcea05f8fcf2362ee34b20bf8ff
Christopher Coffey <christopher.coffey@rackspace.com>
2015-03-20 14:23:59 -0500

    rubocop fix

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,10 +1,11 @@
 if defined?(ChefSpec)
+  # rubocop:disable LineLength
   def install_mysql_chef_gem(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:mysql_chef_gem, :install, resource_name)
   end
-  
+
   def install_mysql2_chef_gem(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:mysql2_chef_gem, :install, resource_name)
   end
-
+  # rubocop:enable LineLength
 end

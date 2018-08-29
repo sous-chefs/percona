3ab7d66c8a6cebf3dba87eb458a77dc7adc009c5
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2018-08-29 15:07:34 -0700

    rm deprecated use_inline_resources

--- libraries/provider_mysql2_chef_gem.rb
+++ libraries/provider_mysql2_chef_gem.rb
@@ -3,8 +3,6 @@ class Chef
     class Mysql2ChefGem
       # Provider to install MySQL2 gem on systems using Percona databases
       class Percona < Chef::Provider::LWRPBase
-        use_inline_resources if defined?(use_inline_resources)
-
         def whyrun_supported?
           true
         end
--- libraries/provider_mysql_chef_gem.rb
+++ libraries/provider_mysql_chef_gem.rb
@@ -3,8 +3,6 @@ class Chef
     class MysqlChefGem
       # Provider to install MySQL gem on systems using Percona databases
       class Percona < Chef::Provider::LWRPBase
-        use_inline_resources if defined?(use_inline_resources)
-
         def whyrun_supported?
           true
         end

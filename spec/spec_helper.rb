d11c1c2cc20d6ee76ccd913a7cfd8be74c5a41e3
Mark E. Schill <Mark.Schill@cmschill.net>
2019-11-06 18:21:42 -0500

    Disables all serverspec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,17 @@
-require 'chefspec'
-require 'chefspec/berkshelf'
+# require 'chefspec'
+# require 'chefspec/berkshelf'
 
-RSpec.configure do |config|
-  config.platform = 'ubuntu'
-  config.version = '16.04'
-  config.log_level = :error
-  config.raise_errors_for_deprecations!
-end
+# RSpec.configure do |config|
+#   config.platform = 'ubuntu'
+#   config.version = '16.04'
+#   config.log_level = :error
+#   config.raise_errors_for_deprecations!
+# end
 
-def add_apt_repository(resource_name)
-  ChefSpec::Matchers::ResourceMatcher.new(:apt_repository, :add, resource_name)
-end
+# def add_apt_repository(resource_name)
+#   ChefSpec::Matchers::ResourceMatcher.new(:apt_repository, :add, resource_name)
+# end
 
-def add_apt_preference(resource_name)
-  ChefSpec::Matchers::ResourceMatcher.new(:apt_preference, :add, resource_name)
-end
+# def add_apt_preference(resource_name)
+#   ChefSpec::Matchers::ResourceMatcher.new(:apt_preference, :add, resource_name)
+# end

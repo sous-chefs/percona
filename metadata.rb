45258a84296e56b882b47be0a66707482f961d87
Tim Smith <tsmith84@gmail.com>
2019-12-06 08:10:51 -0800

    Cookstyle fixes
    Several cookstyle fixes
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs Percona MySQL client and server'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url        'https://github.com/phlipper/chef-percona'
 issues_url        'https://github.com/phlipper/chef-percona/issues'
 version           '0.16.5'

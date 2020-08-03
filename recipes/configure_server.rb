ed2c2034a814ca781e58c58c52b7a71b93c76756
Lance Albertson <lance@osuosl.org>
2020-08-02 21:20:15 -0700

    Disable cookstyle check on node.save (for now)
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- recipes/configure_server.rb
+++ recipes/configure_server.rb
@@ -53,7 +53,7 @@ if server['bind_to']
   ipaddr = Percona::ConfigHelper.bind_to(node, server['bind_to'])
   if ipaddr && server['bind_address'] != ipaddr
     node.override['percona']['server']['bind_address'] = ipaddr
-    node.save unless Chef::Config[:solo]
+    node.save unless Chef::Config[:solo] # rubocop:disable ChefCorrectness/CookbookUsesNodeSave
   end
 
   log "Can't find ip address for #{server['bind_to']}" do

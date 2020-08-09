32c204ecbc80078be34fb2144bcb2384f1dab021
Lance Albertson <lance@osuosl.org>
2020-08-09 11:02:57 -0700

    Additional tests for replication template
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- spec/replication_spec.rb
+++ spec/replication_spec.rb
@@ -24,8 +24,10 @@ describe 'percona::replication' do
 
   describe 'with replication configured' do
     override_attributes['percona']['server']['role'] = ['master']
-    override_attributes['percona']['server']['replication']['password'] = 's3kr1t'
     override_attributes['percona']['server']['root_password'] = 's3kr1t'
+    override_attributes['percona']['server']['replication']['password'] = 's3kr1t'
+    override_attributes['percona']['server']['replication']['username'] = 'replication'
+    override_attributes['percona']['server']['replication']['host'] = 'master-host'
 
     it 'creates a replication template' do
       expect(chef_run).to create_template(replication_sql).with(
@@ -34,7 +36,71 @@ describe 'percona::replication' do
         mode: '0600',
         sensitive: true
       )
-      expect(chef_run).to render_file(replication_sql).with_content('s3kr1t')
+      [
+        /CREATE USER IF NOT EXISTS 'replication'@'%' IDENTIFIED BY 's3kr1t';/,
+        /GRANT REPLICATION SLAVE ON \*\.\* TO 'replication'@'%';/,
+        /MASTER_HOST='master-host'/,
+        /MASTER_USER='replication'/,
+        /MASTER_PASSWORD='s3kr1t'/,
+      ].each do |line|
+        expect(chef_run).to render_file(replication_sql).with_content(line)
+      end
+    end
+
+    context 'version 5.7' do
+      override_attributes['percona']['version'] = '5.7'
+      it do
+        [
+          /GRANT REPLICATION SLAVE ON \*\.\* TO 'replication'@'%' IDENTIFIED BY 's3kr1t';/,
+          /MASTER_HOST='master-host'/,
+          /MASTER_USER='replication'/,
+          /MASTER_PASSWORD='s3kr1t'/,
+        ].each do |line|
+          expect(chef_run).to render_file(replication_sql).with_content(line)
+        end
+      end
+    end
+
+    context 'ssl enabled' do
+      override_attributes['percona']['server']['replication']['ssl_enabled'] = true
+      it do
+        [
+          /ALTER USER 'replication'@'%' REQUIRE SSL;/,
+          /MASTER_SSL=1/,
+          %r{MASTER_SSL_CA='/etc/mysql/ssl/cacert.pem'},
+          %r{MASTER_SSL_CERT='/etc/mysql/ssl/server-cert.pem'},
+          %r{MASTER_SSL_KEY='/etc/mysql/ssl/server-key.pem'},
+        ].each do |line|
+          expect(chef_run).to render_file(replication_sql).with_content(line)
+        end
+      end
+
+      context 'version 5.7' do
+        override_attributes['percona']['version'] = '5.7'
+        it do
+          [
+            /GRANT REPLICATION SLAVE ON *.* TO 'replication'@'%' IDENTIFIED BY 's3kr1t' REQUIRE SSL;/,
+            /MASTER_SSL=1/,
+            %r{MASTER_SSL_CA='/etc/mysql/ssl/cacert.pem'},
+            %r{MASTER_SSL_CERT='/etc/mysql/ssl/server-cert.pem'},
+            %r{MASTER_SSL_KEY='/etc/mysql/ssl/server-key.pem'},
+          ].each do |line|
+            expect(chef_run).to render_file(replication_sql).with_content(line)
+          end
+        end
+      end
+
+      context 'role slave' do
+        override_attributes['percona']['server']['role'] = %w(slave)
+        it do
+          [
+            %r{MASTER_SSL_CERT='/etc/mysql/ssl/client-cert.pem'},
+            %r{MASTER_SSL_KEY='/etc/mysql/ssl/client-key.pem'},
+          ].each do |line|
+            expect(chef_run).to render_file(replication_sql).with_content(line)
+          end
+        end
+      end
     end
 
     it 'executes the replication sql' do
